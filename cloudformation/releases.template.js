3675d6e48f0da7c68c612033b3f5de9d6997e574
ingalls <nick@ingalls.ca>
2024-10-09 14:01:39 -0600

    Add website configuration

--- cloudformation/releases.template.js
+++ cloudformation/releases.template.js
@@ -1,5 +1,10 @@
 import cf from '@openaddresses/cloudfriend';
 
+/**
+ * Note this repo is designed to be managed by a single root administrator
+ * You do not need to deploy or manage this template
+ */
+
 export default cf.merge({
     Description: 'Host TAK Server Release Packages',
     Parameters: {
@@ -9,29 +14,6 @@ export default cf.merge({
         }
     },
     Resources: {
-        AssetBucketPolicy: {
-            Type: 'AWS::S3::BucketPolicy',
-            Properties: {
-                Bucket: cf.ref('AssetBucket'),
-                PolicyDocument: {
-                    Version: '2012-10-17',
-                    Statement: [{
-                        Sid: 'Statement1',
-                        Effect: 'Allow',
-                        Principal: '*',
-                        Action: [
-                            's3:GetObject',
-                            's3:ListObjects',
-                            's3:HeadObject'
-                        ],
-                        Resource: [
-                            cf.join(['arn:', cf.partition, ':s3:::', cf.ref('AssetBucket'), '/*']),
-                            cf.join(['arn:', cf.partition, ':s3:::', cf.ref('AssetBucket')])
-                        ]
-                    }]
-                }
-            }
-        },
         AssetBucket: {
             Type: 'AWS::S3::Bucket',
             Properties: {
@@ -41,6 +23,10 @@ export default cf.merge({
                         ObjectOwnership: 'BucketOwnerEnforced'
                     }]
                 },
+                "WebsiteConfiguration": {
+                    "IndexDocument":"index.html",
+                    "ErrorDocument":"404.html"
+                },
                 PublicAccessBlockConfiguration: {
                     BlockPublicAcls: false,
                     BlockPublicPolicy: false,
@@ -48,6 +34,12 @@ export default cf.merge({
                     RestrictPublicBuckets: false
                 }
             }
+        }
+    },
+    Outputs: {
+        Website: {
+            Description: 'API Root',
+            Value: cf.getAtt('AssetBucket', 'Arn')
         }
     }
 });
