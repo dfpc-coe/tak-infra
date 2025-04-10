18ca410a8936b7df9e4aa624f7e90ac5fdc995b6
ingalls <nick@ingalls.ca>
2025-04-10 10:01:15 -0600

    Add Network Clone

---
+++ cloudformation/network.template.js
@@ -0,0 +1,33 @@
+import cf from '@openaddresses/cloudfriend';
+import DB from './lib/db.js';
+import API from './lib/api.js';
+import Alarms from './lib/alarms.js';
+import KMS from './lib/kms.js';
+import EFS from './lib/efs.js';
+
+// import {
+//    RDS as RDSAlarms
+// } from '@openaddresses/batch-alarms';
+
+export default cf.merge(
+    DB, KMS, Alarms, EFS,
+    {
+        Description: 'Template for @tak-ps/tak-infra',
+        Parameters: {
+            GitSha: {
+                Description: 'GitSha that is currently being deployed',
+                Type: 'String'
+            },
+            Environment: {
+                Description: 'VPC/ECS Stack to deploy into',
+                Type: 'String',
+                Default: 'prod'
+            }
+        }
+    }
+//    RDSAlarms({
+//       prefix: 'Batch',
+//       topic: cf.ref('AlarmTopic'),
+//       instance: cf.ref('DBInstance')
+//    })
+);
