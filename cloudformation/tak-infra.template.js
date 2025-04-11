fecdc51e7a6b9fb2aafaf355ab1e81c7aa432a15
ingalls <nick@ingalls.ca>
2025-04-10 19:54:48 -0600

    Continue splitting stack

--- cloudformation/tak-infra.template.js
+++ cloudformation/tak-infra.template.js
@@ -1,15 +1,9 @@
 import cf from '@openaddresses/cloudfriend';
-import DB from './lib/db.js';
 import API from './lib/api.js';
-import Alarms from './lib/alarms.js';
 import KMS from './lib/kms.js';
-import EFS from './lib/efs.js';
-// import {
-//    RDS as RDSAlarms
-// } from '@openaddresses/batch-alarms';
 
 export default cf.merge(
-    API, DB, KMS, Alarms, EFS,
+    API,
     {
         Description: 'Template for @tak-ps/tak-infra',
         Parameters: {
