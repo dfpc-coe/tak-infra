1c7fae4157b21d64a00f3351c2170c750e149846
ingalls <nick@ingalls.ca>
2025-06-03 15:14:15 -0600

    Update CoreConfig

--- CoreConfig.ts
+++ CoreConfig.ts
@@ -1,7 +1,7 @@
 import * as fs from 'node:fs';
 import * as fsp from 'node:fs/promises';
 import { Static } from '@sinclair/typebox';
-import CoreConfigType from './src/CoreConfigType.js';
+import CoreConfigType from '/opt/tak/scripts/src/CoreConfigType.js';
 import { randomUUID } from 'node:crypto';
 import { toPem } from 'jks-js';
 import { diff } from 'json-diff-ts';
@@ -9,13 +9,11 @@ import { execSync } from 'node:child_process';
 import * as xmljs from 'xml-js';
 
 for (const env of [
-    'HostedDomain',
-    'Environment',
     'PostgresUsername',
     'PostgresPassword',
     'PostgresURL',
     'TAK_VERSION',
-    'LDAP_Domain',
+    'LDAP_DN',
     'LDAP_SECURE_URL'
 ]) {
     if (!process.env[env]) {
@@ -34,14 +32,64 @@ execSync('yes | keytool -import -file /tmp/AmazonRootCA1.pem -alias AWS -deststo
 
 await fsp.copyFile('/tmp/AmazonRootCA1.jks', '/opt/tak/certs/files/aws-acm-root.jks');
 
-const LDAP_DN = process.env.LDAP_Domain.split('.').map((part) => {
-    return `dc=${part}`;
-}).join(',');
+const LetsEncrypt = {
+    Domain: process.env.TAKSERVER_QuickConnect_LetsEncrypt_Domain || 'nodomainset'
+}
 
 const Certificate = {
-    O: process.env.ORGANIZATION || 'COTAK',
-    OU: process.env.ORGANIZATIONAL_UNIT || 'COTAK-Staging'
+    O: process.env.TAKSERVER_CACert_Org || 'TAK',
+    OU: process.env.TAKSERVER_CACert_OrgUnit || 'TAK Unit'
 };
+
+const InputConfig = {
+    Auth: process.env.TAKSERVER_CoreConfig_Network_Input_8089_Auth || 'x509'
+};
+
+const Connector = {
+    EnableAdminUI8443: stringToBoolean(process.env.TAKSERVER_CoreConfig_Network_Connector_8443_EnableAdminUI) || true,
+    EnableNonAdminUI8443: stringToBoolean(process.env.TAKSERVER_CoreConfig_Network_Connector_8443_EnableNonAdminUI) || true,
+    EnableWebtak8443: stringToBoolean(process.env.TAKSERVER_CoreConfig_Network_Connector_8443_EnableWebtak) || true,
+    EnableAdminUI8446: stringToBoolean(process.env.TAKSERVER_CoreConfig_Network_Connector_8446_EnableAdminUI) || true,
+    EnableNonAdminUI8446: stringToBoolean(process.env.TAKSERVER_CoreConfig_Network_Connector_8446_EnableNonAdminUI) || true,
+    EnableWebtak8446: stringToBoolean(process.env.TAKSERVER_CoreConfig_Network_Connector_8446_EnableWebtak) || true
+};
+
+const LDAP_Auth = {
+    X509groups: stringToBoolean(process.env.TAKSERVER_CoreConfig_Auth_X509groups) || true,
+    X509addAnonymous: stringToBoolean(process.env.TAKSERVER_CoreConfig_Auth_X509addAnonymous) || false,
+    X509useGroupCache: stringToBoolean(process.env.TAKSERVER_CoreConfig_Auth_X509useGroupCache) || true,
+    X509useGroupCacheDefaultActive: stringToBoolean(process.env.TAKSERVER_CoreConfig_Auth_X509useGroupCacheDefaultActive) || true,
+    X509checkRevocation: stringToBoolean(process.env.TAKSERVER_CoreConfig_Auth_X509checkRevocation) || true,
+    LDAP_Userstring: process.env.TAKSERVER_CoreConfig_Auth_LDAP_Userstring || 'cn={username},ou=users,',
+    LDAP_Updateinterval: parseInt(process.env.TAKSERVER_CoreConfig_Auth_LDAP_Updateinterval) || 60,
+    LDAP_Groupprefix: process.env.TAKSERVER_CoreConfig_Auth_LDAP_Groupprefix || '',
+    LDAP_GroupNameExtractorRegex: process.env.TAKSERVER_CoreConfig_Auth_LDAP_GroupNameExtractorRegex || 'CN=(.*?)(?:,|$)',
+    LDAP_NestedGroupLookup: stringToBoolean(process.env.TAKSERVER_CoreConfig_Auth_LDAP_NestedGroupLookup) || false,
+    LDAP_Style: process.env.TAKSERVER_CoreConfig_Auth_LDAP_Style || 'DS',
+    LDAP_ServiceAccountDN: process.env.TAKSERVER_CoreConfig_Auth_LDAP_ServiceAccountDN || 'cn=ldapservice,ou=users,',
+    LDAP_UserObjectClass: process.env.TAKSERVER_CoreConfig_Auth_LDAP_UserObjectClass || 'user',
+    LDAP_GroupObjectClass: process.env.TAKSERVER_CoreConfig_Auth_LDAP_GroupObjectClass || 'group',
+    LDAP_DnAttributeName: process.env.TAKSERVER_CoreConfig_Auth_LDAP_DnAttributeName || 'dn',
+    LDAP_NameAttr: process.env.TAKSERVER_CoreConfig_Auth_LDAP_NameAttr || 'cn',
+    LDAP_UserBaseRDN: process.env.TAKSERVER_CoreConfig_Auth_LDAP_UserBaseRDN || 'ou=users,',
+    LDAP_GroupBaseRDN: process.env.TAKSERVER_CoreConfig_Auth_LDAP_GroupBaseRDN || 'ou=groups,',
+    LDAP_CallsignAttribute: process.env.TAKSERVER_CoreConfig_Auth_LDAP_CallsignAttribute || 'takCallsign',
+    LDAP_ColorAttribute: process.env.TAKSERVER_CoreConfig_Auth_LDAP_ColorAttribute || 'takColor',
+    LDAP_RoleAttribute: process.env.TAKSERVER_CoreConfig_Auth_LDAP_RoleAttribute || 'takRole'
+
+};
+
+const Federation = {
+    EnableFederation: stringToBoolean(process.env.TAKSERVER_CoreConfig_Federation_EnableFederation) || true,
+    AllowFederatedDelete: stringToBoolean(process.env.TAKSERVER_CoreConfig_Federation_AllowFederatedDelete) || false,
+    AllowMissionFederation: stringToBoolean(process.env.TAKSERVER_CoreConfig_Federation_AllowMissionFederation) || true,
+    AllowDataFeedFederation: stringToBoolean(process.env.TAKSERVER_CoreConfig_Federation_AllowDataFeedFederation) || true,
+    EnableMissionFederationDisruptionTolerance: stringToBoolean(process.env.TAKSERVER_CoreConfig_Federation_EnableMissionFederationDisruptionTolerance) || true,
+    MissionFederationDisruptionToleranceRecencySeconds: parseInt(process.env.TAKSERVER_CoreConfig_Federation_MissionFederationDisruptionToleranceRecencySeconds) || 43200,
+    EnableDataPackageAndMissionFileFilter: stringToBoolean(process.env.TAKSERVER_CoreConfig_Federation_EnableDataPackageAndMissionFileFilter) || false,
+    Federation_WebBaseUrl: process.env.TAKSERVER_CoreConfig_Federation_WebBaseUrl || 'https://localhost:8443/Marti'
+};
+
 
 const RemoteCoreConfig: Static<typeof CoreConfigType> | null = null;
 let CoreConfig: Static<typeof CoreConfigType> | null = null;
@@ -83,7 +131,7 @@ if (!CoreConfig) {
                 },
                 input: {
                     _attributes: {
-                        auth: 'ldap',
+                        auth: InputConfig.Auth,
                         _name: 'stdssl',
                         protocol: 'tls',
                         port: 8089,
@@ -95,9 +143,11 @@ if (!CoreConfig) {
                         port: 8443,
                         _name: 'https',
                         keystore: 'JKS',
-                        keystoreFile: `/opt/tak/certs/files/${process.env.HostedDomain}/letsencrypt.jks`,
+                        keystoreFile: `/opt/tak/certs/files/${LetsEncrypt.Domain}/letsencrypt.jks`,
                         keystorePass: 'atakatak',
-                        enableNonAdminUI: true
+                        enableNonAdminUI: Connector.EnableNonAdminUI8443,
+                        enableAdminUI: Connector.EnableAdminUI8443,
+                        enableWebtak: Connector.EnableWebtak8443
                     }
                 }, {
                     _attributes: {
@@ -105,9 +155,11 @@ if (!CoreConfig) {
                         clientAuth: false,
                         _name: 'cert_https',
                         keystore: 'JKS',
-                        keystoreFile: `/opt/tak/certs/files/${process.env.HostedDomain}/letsencrypt.jks`,
+                        keystoreFile: `/opt/tak/certs/files/${LetsEncrypt.Domain}/letsencrypt.jks`,
                         keystorePass: 'atakatak',
-                        enableNonAdminUI: true
+                        enableNonAdminUI: Connector.EnableNonAdminUI8446,
+                        enableAdminUI: Connector.EnableAdminUI8446,
+                        enableWebtak: Connector.EnableWebtak8446
                     }
                 }],
                 announce: {
@@ -117,24 +169,32 @@ if (!CoreConfig) {
             auth: {
                 _attributes: {
                     default: 'ldap',
-                    x509groups: true,
-                    x509addAnonymous: false,
-                    x509useGroupCache: true,
-                    x509useGroupCacheDefaultActive: true,
-                    x509checkRevocation: true
+                    x509groups: LDAP_Auth.X509groups,
+                    x509addAnonymous: LDAP_Auth.X509addAnonymous,
+                    x509useGroupCache: LDAP_Auth.X509useGroupCache,
+                    x509useGroupCacheDefaultActive: LDAP_Auth.X509useGroupCacheDefaultActive,
+                    x509checkRevocation: LDAP_Auth.X509checkRevocation
                 },
                 ldap: {
                     _attributes: {
                         url: process.env.LDAP_SECURE_URL,
-                        userstring: `uid={username},ou=People,${LDAP_DN}`,
-                        updateinterval: 60,
-                        groupprefix: '',
-                        groupNameExtractorRegex: 'CN=(.*?)(?:,|$)',
-                        style: 'DS',
-                        serviceAccountDN: `uid=ldapsvcaccount,${LDAP_DN}`,
+                        userstring: LDAP_Auth.LDAP_Userstring + process.env.LDAP_DN,
+                        updateinterval: LDAP_Auth.LDAP_Updateinterval,
+                        groupprefix: LDAP_Auth.LDAP_Groupprefix,
+                        groupNameExtractorRegex: LDAP_Auth.LDAP_GroupNameExtractorRegex,
+                        style: LDAP_Auth.LDAP_Style,
+                        serviceAccountDN: LDAP_Auth.LDAP_ServiceAccountDN + process.env.LDAP_DN,
                         serviceAccountCredential: process.env.LDAP_Password,
-                        groupObjectClass: 'groupOfNames',
-                        groupBaseRDN: `ou=Group,${LDAP_DN}`,
+                        userObjectClass: LDAP_Auth.LDAP_UserObjectClass,
+                        groupObjectClass: LDAP_Auth.LDAP_GroupObjectClass,
+                        groupBaseRDN: LDAP_Auth.LDAP_GroupBaseRDN + process.env.LDAP_DN,
+                        userBaseRDN: LDAP_Auth.LDAP_UserBaseRDN + process.env.LDAP_DN,
+                        dnAttributeName: LDAP_Auth.LDAP_DnAttributeName,
+                        nameAttr: LDAP_Auth.LDAP_NameAttr,
+                        nestedGroupLookup: LDAP_Auth.LDAP_NestedGroupLookup,
+                        callsignAttribute: LDAP_Auth.LDAP_CallsignAttribute,
+                        colorAttribute: LDAP_Auth.LDAP_ColorAttribute,
+                        roleAttribute: LDAP_Auth.LDAP_RoleAttribute,
                         ldapsTruststore: 'JKS',
                         ldapsTruststoreFile: '/opt/tak/certs/files/aws-acm-root.jks',
                         ldapsTruststorePass: 'INTENTIONALLY_NOT_SENSITIVE',
@@ -274,12 +334,55 @@ if (!CoreConfig) {
                     }
                 }
             },
-            locate: {
+            federation: {
                 _attributes: {
-                    enabled: true,
-                    requireLogin: false,
-                    group: 'DEMO - Demonstrations',
-                    mission: 'cotak-locator'
+                    allowFederatedDelete: Federation.AllowFederatedDelete,
+                    allowMissionFederation: Federation.AllowMissionFederation,
+                    allowDataFeedFederation: Federation.AllowDataFeedFederation,
+                    enableMissionFederationDisruptionTolerance: Federation.EnableMissionFederationDisruptionTolerance,
+                    missionFederationDisruptionToleranceRecencySeconds: Federation.MissionFederationDisruptionToleranceRecencySeconds,
+                    enableFederation: Federation.EnableFederation,
+                    enableDataPackageAndMissionFileFilter: Federation.EnableDataPackageAndMissionFileFilter
+                },
+                'federation-server': {
+                    _attributes: {
+                        port: 9000,
+                        coreVersion: 2,
+                        v1enabled: false,
+                        v2port: 9001,
+                        v2enabled: true,
+                        webBaseUrl: Federation.Federation_WebBaseUrl,
+                    },
+                    tls: {
+                        _attributes: {
+                            keystore: 'JKS',
+                            keystoreFile: '/opt/tak/certs/files/takserver.jks',
+                            keystorePass: 'atakatak',
+                            truststore: 'JKS',
+                            truststoreFile: '/opt/tak/certs/files/fed-truststore.jks',
+                            truststorePass: 'atakatak',
+                            context: 'TLSv1.2',
+                            keymanager: 'SunX509'
+                        }
+                    },
+                    'federation-port': {
+                        _attributes: {
+                            port: 9000,
+                            tlsVersion: 'TLSv1.2'
+                        }
+                    },
+                    v1Tls: [{
+                        _attributes: {
+                            tlsVersion: 'TLSv1.2'
+                        }
+                    },{
+                        _attributes: {
+                            tlsVersion: 'TLSv1.3'
+                        }
+                    }]
+                },
+                fileFilter: {
+                    fileExtension: ['pref']
                 }
             },
             plugins: {},
@@ -339,18 +442,18 @@ const xml = xmljs.js2xml(CoreConfig, {
 });
 
 fs.writeFileSync(
-    './CoreConfig.xml',
+    '/opt/tak/CoreConfig.xml',
     `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n${xml}`
 );
 
 try {
-    console.log('ok - Checking for Diff in CoreConfig');
+    console.log('ok - TAK Server - Checking for Diff in CoreConfig.xml');
     const diffs = diff(RemoteCoreConfig, CoreConfig);
 
     if (diffs.length > 0) {
-        console.log('ok - Change detected');
+        console.log('ok - TAK Server - CoreConfig.xml change detected');
     } else {
-        console.log('ok - No change detected');
+        console.log('ok - TAK Server - No CoreConfig.xml change detected');
     }
 } catch (err) {
     console.error(err);
@@ -360,4 +463,8 @@ function validateKeystore(file, pass) {
     fs.accessSync(file);
     const jksBuffer = fs.readFileSync(file);
     toPem(jksBuffer, pass);
+}
+
+function stringToBoolean(str: string): boolean {
+    return str.toLowerCase() === 'true';
 }
